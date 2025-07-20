a367c4d3a09e8d24f1e64f7eca8ffe14a51c8ccf
Saniya coder <khatiqsaniya1@gmail.com>
2025-07-20 08:02:23 +0530

    Update 01_variables.py
    My daily Python practice repo — learning by doing.

--- 01_variables.py
+++ 01_variables.py
@@ -1,1 +1,8 @@
 print("hello world")
+#Practicing user input and output 💬
+
+name = input("What's your name? ")
+age = input("How old are you? ")
+
+print("Nice to meet you, " + name + "!")
+print("You are " + age + " years old.")
