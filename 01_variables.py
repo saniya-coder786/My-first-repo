7fa3759d878c370ef66b188ef81d22afa16065e5
Saniya coder <khatiqsaniya1@gmail.com>
2025-09-15 17:00:29 +0530

    Update 01_variables.py
    Mistake correction

--- 01_variables.py
+++ 01_variables.py
@@ -8,9 +8,6 @@ print("Nice to meet you, " + name + "!")
 print("You are " + age + " years old.")
 
 # next step practice python 
-
-hobby = input("What's your favorite hobby? ")
-food = input("What's your favorite food? ")
 #veriables
 name="saniya"# string
 age=20 #integer
